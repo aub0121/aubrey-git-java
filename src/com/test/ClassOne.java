e4e166bbc273a2ec854b18684149d58cb0a57a61
Aubrey Ruiz <aubreyannaruiz@gmail.com>
2023-05-16 21:55:21 -0400

    Class One

---
+++ src/com/test/ClassOne.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class ClassOne {
+	public static void main(String[] args) {
+		System.out.println("Print something");
+	}
+
+}
---
+++ src/com/test/ClassTwo.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class ClassTwo {
+	public static void main(String[] args) {
+		System.out.println("Print something");
+	}
+
+}
